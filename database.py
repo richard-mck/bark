08a850e743cc5bf87a5140f46149025e33f42c8e
rmck <richard.a.mck@gmail.com>
2024-06-25 14:31:00 -0700

    feat: reusable SQL statement execution as a transaction

--- database.py
+++ database.py
@@ -9,3 +9,11 @@ class DatabaseManager:
 
     def __del__(self):
         self.connection.close()
+
+    def _execute(self, statement: str, values=None):
+        """Given an SQL statement and values, execute it, returning the cursor containing the result"""
+        with self.connection:
+            # Using `with` here ensures execution occurs as a transaction
+            cursor = self.connection.cursor()
+            cursor.execute(statement, values or [])
+            return cursor
