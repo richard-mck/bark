466595663f9d1c8507a17ca297b3862e6ae72c2b
rmck <richard.a.mck@gmail.com>
2024-08-23 16:21:40 -0700

    feat: add ability to create initial table and add items

--- commands.py
+++ commands.py
@@ -1,3 +1,26 @@
+from datetime import datetime
+
 from database import DatabaseManager
 
 db = DatabaseManager("bookmarks.db")
+
+
+class CreateBookmarksTableCommand:
+    def execute(self):
+        db.create_table(
+            "bookmarks",
+            {
+                "id": "INTEGER PRIMARY KEY AUTOINCREMENT",
+                "title": "TEXT NOT NULL",
+                "url": "TEXT NOT NULL",
+                "notes": "TEXT",
+                "date_added": "TEXT NOT NULL",
+            },
+        )
+
+
+class AddBookmarksCommand:
+    def execute(self, data: dict[str, str]) -> str:
+        data["date_added"] = datetime.utcnow().isoformat()
+        db.add("bookmarks", data)
+        return f"Successfully added '{data['title']}' to bookmarks"
