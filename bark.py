6c0f76a4fbf48f85926b407df86d9040e82e87c6
rmck <richard.a.mck@gmail.com>
2024-08-23 17:15:47 -0700

    feat: setup text options storage block

--- bark.py
+++ bark.py
@@ -2,6 +2,24 @@
 
 import commands
 
+
+class Options:
+    """Class to contain menu options for display"""
+
+    def __init__(self, display_name: str, command, preparation=None):
+        self.display_name = display_name
+        self.command = command
+        self.preparation = preparation
+
+    def choose(self):
+        data = self.preparation() if self.preparation else None
+        message = self.command.execute(data) if data else self.command.execute()
+        print(message)
+
+    def __str__(self):
+        return self.display_name
+
+
 if __name__ == "__main__":
     print("Welcome to Bark!")
     commands.CreateBookmarksTableCommand().execute()
